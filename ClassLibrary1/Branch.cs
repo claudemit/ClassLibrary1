6610002d6835db93ba4556b87cd2230442ac5bd5
panhao4812 <29347213@qq.com>
2013-08-21 19:48:22 +0800

    2013082119

--- ClassLibrary1/Branch.cs
+++ ClassLibrary1/Branch.cs
@@ -2,14 +2,12 @@
 using Rhino.Geometry;
 using Rhino.DocObjects;
 using Rhino.Collections;
-
 using GH_IO;
 using GH_IO.Serialization;
 using Grasshopper;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Data;
 using Grasshopper.Kernel.Types;
-
 using System;
 using System.IO;
 using System.Xml;
@@ -25,7 +23,7 @@ using System.Runtime.InteropServices;
 
 namespace RhinoDebug
 {
-    class RhinoDebugDemo:GH_ScriptInstance
+    class Branch:GH_ScriptInstance
     {
         #region Members
         /// <summary>List of error messages. Do not modify this list directly.</summary>
@@ -79,48 +77,87 @@ namespace RhinoDebug
 
         #endregion
 
-        private void RunScript(List<Mesh> x, List<int> y, object z, ref object A, ref object B, ref object C)
+        private void RunScript(List<Mesh> x, List<int> y,  List<double>  z, ref object A, ref object B, ref object C,ref object D)
         {
             try
             {
-
-                List<Line> output1 = new List<Line>();
-                List<double> output2 = new List<double>();
-                List<Point3d> output3 = new List<Point3d>();
-                for (int i = 0; i < x.Count; i++)
+                DataTree<double> output222;
+                if (temp.Count == 0)
                 {
-                    List<Line> output11;
-                    List<double> output22;
-                    Point3d output33;
-                    Print(Step(x[i], y[i], 3, true, out output11, out output22, out output33));
-                    output1.AddRange(output11);
-                    output2.AddRange(output22);
-                    output3.Add(output33);
+                    temp = y;
+                    for (int i = 0; i < x.Count; i++)
+                    {
+                        GH_Path path = new GH_Path(i);
+                        List<Line> output11;
+                        List<double> output22;
+                        Point3d output33;
+                        List<Point3d> output44;
+                        Print(Step(x[i], y[i], 0.5, true, out output11, out output22, out output33, out output44));
+                        output1.AddRange(output11, path);
+                        output2.AddRange(output22, path);
+                        output3.Add(output33, path);
+                        output4.AddRange(output44, path);
+                    }
                 }
-                A = output1; B = output2; C = output3;
+                else
+                {
+                    for (int i = 0; i < y.Count; i++)
+                    {
+                        GH_Path path = new GH_Path(i);
+                        if (y[i] != temp[i])
+                        {
+                            List<Line> output11;
+                            List<double> output22;
+                            Point3d output33;
+                            List<Point3d> output44;
+                            Print(Step(x[i], y[i], 0.5, true, out output11, out output22, out output33, out output44));
+                            output1.Branch(path).Clear();
+                            output1.Branch(path).AddRange(output11);
+                            output2.Branch(path).Clear();
+                            output2.Branch(path).AddRange(output22);
+                            output3.Branch(path)[0] = output33;
+                            output4.Branch(path).Clear();
+                            output4.Branch(path).AddRange(output44);
+                            temp[i] = y[i];
+                        }
+                    }
+                }
+
+                output222 = new DataTree<double>(output2);
+                for (int i = 0; i < output222.Paths.Count; i++)
+                {
+                    GH_Path path = new GH_Path(i);
+                    for(int j=0;j<output222.Branch(path).Count;j++){
+                        output222[path, j] *= z[i];
+                }}
+                A = output1; B = output222; C = output3; D = output4;
             }
             catch (Exception ex)
             {
                 Print(ex.ToString());
             }
-
         }
         ////////////////////////////////////////////////////////////////////////////////////
         //代码
-
-        public String Step(Mesh mesh, int age, double dis, bool type, out List<Line> out1, out List<double> out2, out Point3d IO)
+        List<int> temp = new List<int>();
+        DataTree<Line> output1 = new DataTree<Line>();
+        DataTree<double> output2 = new DataTree<double>();
+        DataTree<Point3d> output3 = new DataTree<Point3d>();
+        DataTree<Point3d> output4 = new DataTree<Point3d>();
+        public String Step(Mesh mesh, int age, double dis, bool type, out List<Line> out1, out List<double> out2, out Point3d IO, out List<Point3d> IO2)
         {
             string str = "";
-            List<Line> output1 = new List<Line>();
-            List<double> output2 = new List<double>();
+            out1 = new List<Line>();
+            out2 = new List<double>();
             IO = new Point3d();
+            IO2 = new List<Point3d>();
             if (mesh.Vertices.Count < 4)
             {
-                str = "Mesh vertices Count<4"; out1 = output1; out2 = output2; return str;
+                str = "Mesh vertices Count<4"; return str;
             }
             if (age < 2)
             {
-                str = "age<2"; out1 = output1; out2 = output2; return str;
+                str = "age<2";  return str;
             }
             mesh.FaceNormals.ComputeFaceNormals();
             Polyline pl = new Polyline();
@@ -128,10 +165,7 @@ namespace RhinoDebug
             pl.Add(mesh.Vertices[1]);
             pl.Add(mesh.Vertices[2]);
             pl.Add(mesh.Vertices[3]);
-
-
             List<List<box>> tree = new List<List<box>>();
-
             for (int i = 0; i < age; i++)
             {
                 List<box> branch = new List<box>();
@@ -141,21 +175,24 @@ namespace RhinoDebug
                 Loop(ref branch, age - i - 1, type);
                 tree.Add(branch);
             }
-
             for (int i = 1; i < age; i++)
             {
                 List<box> branch2 = tree[i - 1];
                 List<box> branch = tree[i];
                 for (int j = 0; j < branch.Count; j++)
                 {
-                    output1.Add(new Line(branch[j].p, branch2[j * 2].p));
-                    output2.Add(branch[j].age);
-                    output1.Add(new Line(branch[j].p, branch2[j * 2 + 1].p));
-                    output2.Add(branch[j].age);
+                    out1.Add(new Line(branch[j].p, branch2[j * 2].p));
+                    out2.Add(branch[j].age);
+                    out1.Add(new Line(branch[j].p, branch2[j * 2 + 1].p));
+                    out2.Add(branch[j].age);
                 }
             }
-
-            out1 = output1; out2 = output2; IO = tree[tree.Count - 1][0].p;
+           IO = tree[tree.Count - 1][0].p;
+            for(int i=0;i<tree[0].Count;i++){
+                IO2.Add(tree[0][i].p);
+            }
+            str += "Branch Count= " + age.ToString() + "\r\n";
+            str += "Success";
             return str;
         }
         public void Loop(ref List<box> b, int step, bool type)
@@ -181,6 +218,7 @@ namespace RhinoDebug
         {
             public double age = 100;
             public Point3d p = new Point3d();
+            private Random rnd = new Random();
             public box(Polyline l, double Age)
             {
                 this.age = Age;
@@ -262,17 +300,19 @@ namespace RhinoDebug
                 Polyline l1 = new Polyline();
                 Polyline l2 = new Polyline();
                 Point3d p4, p5;
+                double t1 = rnd.NextDouble() * 0.3 + 0.35;
+                double t2 = rnd.NextDouble() * 0.3 + 0.35;
                 if (p0.DistanceTo(p1) > p1.DistanceTo(p2))
                 {
-                    p4 = p0 * 0.5 + p1 * 0.5;
-                    p5 = p3 * 0.5 + p2 * 0.5;
+                    p4 = p0 * t1 + p1 * (1 - t1);
+                    p5 = p3 * t2 + p2 * (1 - t2);
                     l1.Add(p0); l1.Add(p4); l1.Add(p5); l1.Add(p3);
                     l2.Add(p4); l2.Add(p1); l2.Add(p2); l2.Add(p5);
                 }
                 else
                 {
-                    p4 = p0 * 0.5 + p3 * 0.5;
-                    p5 = p1 * 0.5 + p2 * 0.5;
+                    p4 = p0 * t1 + p3 * (1 - t1);
+                    p5 = p1 * t2 + p2 * (1 - t2);
                     l1.Add(p0); l1.Add(p1); l1.Add(p5); l1.Add(p4);
                     l2.Add(p4); l2.Add(p5); l2.Add(p2); l2.Add(p3);
                 }
@@ -280,8 +320,6 @@ namespace RhinoDebug
                 return output;
             }
         }
-
-
         ////////////////////////////////////////////////////////////////////////////////////
     }
 }
